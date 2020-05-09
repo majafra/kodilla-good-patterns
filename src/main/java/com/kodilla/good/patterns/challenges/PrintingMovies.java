159663da10bc502eeb9c37ee6a3713580e489481
majafra <bartlomiej.wit@gmail.com>
2020-05-09 23:16:53 +0200

    first commit

---
+++ src/main/java/com/kodilla/good/patterns/challenges/PrintingMovies.java
@@ -0,0 +1,17 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class PrintingMovies {
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        String result = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .map(s->s.toString())
+                .collect(Collectors.joining("!", "", ""));
+
+        System.out.println(result);
+    }
+}
